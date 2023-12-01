dfc1dd22d03691a4b4a4a40f3447201e6bb5bfc7
jibonrahaman <manvirjibon@gmail.com>
2023-12-01 10:57:08 +0600

    Todo About Page design

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -6,10 +6,12 @@ function HomePage() {
   return (
     <div  >
  <div>
-  <div className='flex'>
-  <SideBar className=" w-[250px]"/>
+  <div className='flex '>
+  <SideBar className=" w-[17%]"/>
   <div class="border-r-[1px] border-[#212121]"></div>
- <TodoAbout />
+<div className='bg-[#000000] w-[83%] '>
+<TodoAbout  />
+</div>
   </div>
  </div>
   
--- src/pages/TodoAbout/TodoAbout.jsx
+++ src/pages/TodoAbout/TodoAbout.jsx
@@ -3,13 +3,13 @@ import Product from '../../assets/Product4.png'
 import Flex from '../../Components/Flex'
 function TodoAbout() {
   return (
-    <section>
-        <div className=' h-screen px-36 pt-14' >
-               <Flex className="   ">
+    <section className=' '>
+        <div className=' h-screen text-white  px-44 pt-10' >
+               <Flex className="  items-center gap-x-20">
                 <div>
                     <img src={Product} alt={Product} className=' w[200px] h-[200px] rounded-full' />
                 </div>
-                <div className=' absolute top-[90px] left-[650px]'>
+                <Flex className=' flex-col'>
                 <Flex className=" items-center gap-x-8">
                     <h2 className=' font-text text-2xl'>Jibon</h2>
                     <p className=' px-4 py-2 bg-[#131313] hover:bg-[#363636] duration-300 rounded-lg text-white '>Edite Profile</p>
@@ -20,8 +20,16 @@ function TodoAbout() {
                    <p>64 followers</p>
                    <p>4 following</p>
                 </Flex>
-                </div>
+                </Flex>
              </Flex>
+
+             <div className=' w-[520px] px-4 py-4 bg-[#242526] ml-28'>
+        <Flex className=" gap-x-4">
+        <img src={Product} alt={Product} className='w-[50px] h-[50px] rounded-full' />
+        <input type="text" placeholder="What's your mind?" className='w-[400px] rounded-2xl px-3 hover:bg-[#4e4f50] bg-[#3a3b3c] focus:outline-none' />
+        </Flex>
+        <div className='border border-[#37383a]'></div>
+            </div>
             
             </div>
    
