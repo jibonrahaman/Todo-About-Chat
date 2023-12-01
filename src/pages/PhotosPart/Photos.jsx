34dee20ab849f564a6e43bc205d3dbed59f23910
jibonrahaman <manvirjibon@gmail.com>
2023-12-01 19:08:43 +0600

    react router path photos

---
+++ src/pages/PhotosPart/Photos.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Photos() {
+  return (
+    <div>
+      <h2>asd</h2>
+    </div>
+  )
+}
+
+export default Photos
--- src/pages/PostPart/Post.jsx
+++ src/pages/PostPart/Post.jsx
@@ -1,23 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { CiSignpostDuo1 } from "react-icons/ci";
 import Flex from '../../Components/Flex'
 import { SiImagej } from "react-icons/si";
 
 function Post() {
+  const [post,setpost]=useState(false);
+  const [photos,setphotos]=useState(false);
+  const handlePost =()=>{
+    setpost(true)
+  }
+  const handlePhotos = () =>{
+    setphotos(true)
+  }
   return (
-  <section className=' mt-10'>
+  <section className=' mt-14'>
     <div  className=' border-t-2 border-[#212121] relative '>
-        <div className='bg-red-400 w-12 h-[2px] absolute top-0  left-[50%]'></div>
+        <div className={`bg-[#bdbdbd] w-16 h-[2px] absolute top-0  left-[39%]`}></div>
     </div>
   <Flex className="justify-center gap-x-14 mt-3">
-  <Flex className=" gap-x-2  items-center">
+<div onClick={handlePost}>
+<Flex className=" gap-x-2  items-center">
    <CiSignpostDuo1 />
    <span >POSTS</span>
    </Flex>
-   <Flex className="  items-center gap-x-2">
+</div>
+  <div onClick={handlePhotos}>
+  <Flex className="  items-center gap-x-2">
    <SiImagej />
        <span>PHOTOS</span>
    </Flex>
+  </div>
   </Flex>
   </section>
   )
